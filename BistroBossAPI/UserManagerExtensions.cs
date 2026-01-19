f933aed8a3534230aa501d5f20de736dfe88513f
siatkabiedy <kamilopa6@gmail.com>
2026-01-19 12:04:26 +0100

    Dodaje metody rozszerzające UserManagera, pozwala sprawdzic czy user jest administratorem i czy ma dostęp do endpointu.

---
+++ BistroBossAPI/UserManagerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BistroBossAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace BistroBossAPI
+{
+    public static class UserManagerExtensions
+    {
+        public static bool IsNotAllowedInEndpoint(this UserManager<Uzytkownik> userManager, string userId, ClaimsPrincipal claims)
+        {
+            return claims.Identity.IsAuthenticated && userManager.GetUserId(claims) != userId;
+        }
+
+        public static async Task<bool> IsAdminAsync(this UserManager<Uzytkownik> userManager, ClaimsPrincipal claims)
+        {
+            var user = await userManager.GetUserAsync(claims);
+            return user?.AccessLevel == 2;
+        }
+    }
+}
