b6c921e5bf6ab34775e07448c4f8e81d477abc79
Rafał Wronowski <raf.wro42@gmail.com>
2026-01-28 20:36:25 +0100

    Przywróciłem TestControllerAPI do projektu.

---
+++ BistroBossAPI/Controllers/ApiControllers/TestControllerAPI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BistroBossAPI.Controllers.ApiControllers
+{
+    [Route("api/authtest")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class TestControllerAPI : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(new string[] { "Ok" });
+        }
+    }
+}
