d8fa926b2f4ff7fd81d53f4da2b8e03cfd627779
Your Name <you@example.com>
2026-01-28 14:38:31 +0100

    Poprawki w końcówkach.
    Dodaje sprawdzenie czy użytkownik jest adminem dla każdej końcówki oraz dodaje inne kody błędów (wcześniej zwracało tylko kod 200 Ok, teraz może 401 Unauthorized i 404 NotFound)

--- BistroBossAPI/Controllers/ApiControllers/ManageControllerAPI.cs
+++ BistroBossAPI/Controllers/ApiControllers/ManageControllerAPI.cs
@@ -1,7 +1,9 @@
-﻿using BistroBossAPI.Models.Dto;
+﻿using BistroBossAPI.Models;
+using BistroBossAPI.Models.Dto;
 using BistroBossAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BistroBossAPI.Controllers.ApiControllers
@@ -12,21 +14,33 @@ namespace BistroBossAPI.Controllers.ApiControllers
     public class ManageControllerAPI : ControllerBase
     {
         private readonly ManageService _service;
+        private readonly UserManager<Uzytkownik> _userManager;
 
-        public ManageControllerAPI(ManageService service)
+        public ManageControllerAPI(ManageService service, UserManager<Uzytkownik> userManager)
         {
             _service = service;
+            _userManager = userManager;
         }
 
         [HttpGet("orders")]
         public async Task<IActionResult> GetAllOrders([FromQuery] int? search)
         {
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
             return Ok(await _service.GetAllOrdersAsync(search));
         }
 
         [HttpGet("orders/{id}")]
         public async Task<IActionResult> GetOrder(int id)
         {
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
             var result = await _service.GetOrderAsync(id);
             return result == null ? NotFound() : Ok(result);
         }
@@ -34,55 +48,124 @@ namespace BistroBossAPI.Controllers.ApiControllers
         [HttpPut("orders/{id}/cancel")]
         public async Task<IActionResult> Cancel(int id)
         {
-            return Ok(await _service.SetStatusAsync(id, 0));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetStatusAsync(id, 0);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpPut("orders/{id}/prepare")]
         public async Task<IActionResult> Prepare(int id)
         {
-            return Ok(await _service.SetStatusAsync(id, 2));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetStatusAsync(id, 2);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpPut("orders/{id}/delivery")]
         public async Task<IActionResult> Delivery(int id)
         {
-            return Ok(await _service.SetInDeliveryAsync(id));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetInDeliveryAsync(id);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpPut("orders/{id}/complete")]
         public async Task<IActionResult> Complete(int id)
         {
-            return Ok(await _service.SetStatusAsync(id, 4));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetStatusAsync(id, 4);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpDelete("orders/{id}")]
         public async Task<IActionResult> DeleteOrder(int id)
         {
-            return Ok(await _service.DeleteOrderAsync(id));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.DeleteOrderAsync(id);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpGet("users")]
         public async Task<IActionResult> GetUsers()
         {
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
             return Ok(await _service.GetUsersAsync());
         }
 
         [HttpPut("users/{id}/admin")]
         public async Task<IActionResult> MakeAdmin(string id)
         {
-            return Ok(await _service.SetAdminAsync(id, true));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetAdminAsync(id, true);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpPut("users/{id}/unadmin")]
         public async Task<IActionResult> UnmakeAdmin(string id)
         {
-            return Ok(await _service.SetAdminAsync(id, false));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.SetAdminAsync(id, false);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
 
         [HttpDelete("users/{id}")]
         public async Task<IActionResult> RemoveUser(string id)
         {
-            return Ok(await _service.RemoveUserAsync(id));
+            if (!await _userManager.IsAdminAsync(User))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _service.RemoveUserAsync(id);
+            if (!result.Success)
+                return NotFound(result);
+            return Ok(result);
         }
     }
 }
